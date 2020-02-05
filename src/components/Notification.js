dc1bcc5780c28c83d421aa92706eb8a0cc5dd5c6
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-02-05 13:56:42 +0200

    Hide notificaction logic

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const Notification = ({ message, isSuccess }) => (
-  <div
-    className={`fixed flex justify-between items-center top-0 right-0 w-full text-gray-100 z-50 p-3
-                ${isSuccess ? 'bg-indigo-600' : 'bg-red-500'}`}
-  >
-    <p>{message}</p>
-    <button type="button" className="text-xl focus:outline-none">⨯</button>
-  </div>
-);
+const Notification = ({ message, isSuccess }) => {
+  const [isHidden, setIsHidden] = useState(false);
+  return (
+    <div
+      className={`fixed justify-between items-center top-0 right-0 w-full text-gray-100 z-50 p-3
+                ${isSuccess ? 'bg-indigo-600' : 'bg-red-500'}
+                ${isHidden ? 'hidden' : 'flex'}`}
+    >
+      <p>{message}</p>
+      <button
+        type="button"
+        onClick={() => setIsHidden(true)}
+        className="text-xl focus:outline-none"
+      >
+        ⨯
+      </button>
+    </div>
+  );
+};
 
 Notification.propTypes = {
   message: PropTypes.string.isRequired,

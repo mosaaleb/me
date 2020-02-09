ea89cf63d6b7489ca8e2532b74cc16138bc28809
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-02-09 15:03:03 +0200

    add purgecss to config for production bundle

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,16 @@
 module.exports = {
   plugins: [
-    'gatsby-plugin-postcss'
+    'gatsby-plugin-postcss',
+    {
+      resolve: 'gatsby-plugin-purgecss',
+      options: {
+        printRejected: true,
+        develop: false,
+        tailwind: true
+        // whitelist: ['whitelist'],
+        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'],
+        // purgeOnly : ['components/', '/main.css', 'bootstrap/'],
+      }
+    }
   ]
 };

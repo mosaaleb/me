e8e997f08ec50a3f17362cb35bb982992701230d
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-02-11 15:14:55 +0200

    - move sidebar state to layout component - fix sidebar in small screen

--- src/layouts/SideBar.js
+++ src/layouts/SideBar.js
@@ -1,39 +1,33 @@
-import React, { useState } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import NavLinks from '../components/NavLinks';
 import SocialLinks from '../components/SocialLinks';
 
-const SideBar = () => {
-  // const isMobile = window.innerWidth < 480;
-  const [, setIsClosed] = useState(false);
-  return (
-    <div
-      className={`bg-pattern fixed hidden flex flex-col w-4/5 justify-between h-screen border-r-2 border-gray-900
-                 sm:static lg:flex lg:w-3/13 lg:max-w-sm`}
-    >
-      <div>
-        <button
-          type="button"
-          onClick={() => setIsClosed(true)}
-          className="block ml-auto p-5 focus:outline-none hover:text-indigo-600 lg:invisible"
-        >
-          <svg className="h-6 w-6 fill-current">
-            <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" />
-          </svg>
-        </button>
-        <NavLinks />
-      </div>
-      <SocialLinks />
-      <button type="button" className="fixed p-5 block focus:outline-none hover:text-indigo-600 lg:invisible z-50">
-        <svg
-          className="h-6 w-6 fill-current"
-        >
-          <path
-            d="M22 7V5H2V7H22ZM22 11V13H2V11H22ZM22 17V19H2V17H22Z"
-          />
+const SideBar = ({ isSideBarOpen, setIsSideBarOpen }) => (
+  <div
+    className={`bg-pattern h-screen z-50 border-r-2 border-gray-900 w-4/5 flex flex-col justify-between
+                ${isSideBarOpen ? 'fixed' : 'hidden'}
+                lg:w-3/13 lg:max-w-sm lg:static`}
+  >
+    <div>
+      <button
+        type="button"
+        onClick={() => setIsSideBarOpen(false)}
+        className="block ml-auto p-5 focus:outline-none hover:text-indigo-600 lg:invisible"
+      >
+        <svg className="h-6 w-6 fill-current">
+          <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" />
         </svg>
       </button>
+      <NavLinks />
     </div>
-  );
+    <SocialLinks />
+  </div>
+);
+
+SideBar.propTypes = {
+  isSideBarOpen: PropTypes.bool.isRequired,
+  setIsSideBarOpen: PropTypes.func.isRequired
 };
 
 export default SideBar;

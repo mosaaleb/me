0aa578d369370da766bbe751598bfff37d897ee0
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-02-13 21:06:13 +0200

    style blog post hero section

--- src/templates/Post.js
+++ src/templates/Post.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
+import Helmet from 'react-helmet';
+import Image from 'gatsby-image';
 import PropTypes from 'prop-types';
 import Layout from '../layouts/Layout';
 import TagLinks from '../components/TagLinks';
@@ -11,6 +13,13 @@ export const query = graphql`
       frontmatter {
         date
         title
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
         author
         categories
       }
@@ -21,6 +30,10 @@ export const query = graphql`
 
 const Post = ({ data: { mdx: post } }) => (
   <Layout>
+    <Helmet>
+      <html lang="en" />
+      <title>{post.frontmatter.title}</title>
+    </Helmet>
     <div className="mx-auto md:w-11/12 max-w-6xl">
       <TagLinks tags={post.frontmatter.categories} />
       <h2 className="font-bold text-2xl lg:text-4xl text-center text-indigo-900 my-4">
@@ -30,6 +43,13 @@ const Post = ({ data: { mdx: post } }) => (
         Posted on&nbsp;
         {new Date(post.frontmatter.date).toDateString()}
       </h4>
+      <div className="rounded my-6 max-w-xl mx-auto overflow-hidden">
+        <Image
+          alt={post.frontmatter.title}
+          fluid={post.frontmatter.image.childImageSharp.fluid}
+          className="cover"
+        />
+      </div>
       <div className="max-w-3xl mx-auto my-10">
         <MDXRenderer>{post.body}</MDXRenderer>
       </div>

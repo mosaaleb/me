c680ade099fb8f95ab7a7331f4c790b4ced53f7a
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-02-28 22:57:22 +0200

    fix linter errors

--- src/components/content/Projects.js
+++ src/components/content/Projects.js
@@ -8,7 +8,8 @@ const Projects = () => {
     <div className="py-4">
       <h3
         id="projects"
-        className="font-bold text-4xl text-indigo-600 border-b inline-block border-dashed border-gray-500">
+        className="font-bold text-4xl text-indigo-600 border-b inline-block border-dashed border-gray-500"
+      >
         Projects
       </h3>
       <div>

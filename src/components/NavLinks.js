a34b63e980250841ad43b4253dcab2ed3b097116
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-04-07 23:13:53 +0200

    close sidebar when link is clicked

--- src/components/NavLinks.js
+++ src/components/NavLinks.js
@@ -1,27 +1,32 @@
 import React from 'react';
+import PropTyes from 'prop-types';
 import { Link } from 'gatsby';
 
-const NavLinks = () => (
+const NavLinks = ({ setIsSideBarOpen }) => (
   <nav className="text-2xl px-10">
     <p className="p-2 hover:underline hover:text-indigo-600">
-      <Link to="/">Home</Link>
+      <Link to="/" onClick={() => setIsSideBarOpen(false)}>Home</Link>
     </p>
     <p className="p-2 hover:underline hover:text-indigo-600">
-      <Link to="#about">About</Link>
+      <Link to="#about" onClick={() => setIsSideBarOpen(false)}>About</Link>
     </p>
     <p className="p-2 hover:underline hover:text-indigo-600">
-      <Link to="#projects">Projects</Link>
+      <Link to="#projects" onClick={() => setIsSideBarOpen(false)}>Projects</Link>
     </p>
     <p className="p-2 hover:underline hover:text-indigo-600">
-      <Link to="#skills">Skills</Link>
+      <Link to="#skills" onClick={() => setIsSideBarOpen(false)}>Skills</Link>
     </p>
     <p className="p-2 hover:underline hover:text-indigo-600">
-      <Link to="#contact">Contact</Link>
+      <Link to="#contact" onClick={() => setIsSideBarOpen(false)}>Contact</Link>
     </p>
     <p className="p-2 hover:underline hover:text-indigo-600">
       <Link to="/blog">Blog</Link>
     </p>
   </nav>
 );
+
+NavLinks.propTypes = {
+  setIsSideBarOpen: PropTyes.func.isRequired
+};
 
 export default NavLinks;

2627922ace3a7e5c62e7734b9f13cf6b5eb1861b
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-04-08 00:39:36 +0200

    fix linting errors

--- src/layouts/PrimaryView.js
+++ src/layouts/PrimaryView.js
@@ -2,18 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Footer from '../components/Footer';
 
-const PrimaryView = ({ children }) => {
-  return (
-    <main className="flex-1 flex flex-col overflow-hidden">
-      <div className="px-5 flex-1 overflow-y-scroll focus:outline-none">
-        <div className="flex flex-col min-h-screen">
-          <div className="flex-grow py-16">{children}</div>
-          <Footer />
-        </div>
+const PrimaryView = ({ children }) => (
+  <main className="flex-1 flex flex-col overflow-hidden">
+    <div className="px-5 flex-1 overflow-y-scroll">
+      <div className="flex flex-col min-h-screen">
+        <div className="flex-grow py-16">{children}</div>
+        <Footer />
       </div>
-    </main>
-  );
-};
+    </div>
+  </main>
+);
+
 
 PrimaryView.propTypes = {
   children: PropTypes.node.isRequired

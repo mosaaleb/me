8484e00f9ba2cf72c6d27a69d393984d01131e3a
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-04-16 18:18:12 +0200

    change footer style add netlify footnote

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
 const Footer = () => (
-  <div className="text-xl pb-5 text-center text-indigo-600 underline">
-    Muhammad Ebeid ©
-    {' '}
-    {new Date().getFullYear()}
+  <div className="pb-10 text-sm text-center">
+    <p>
+      ©
+      {' '}
+      {new Date().getFullYear()}
+      {' '}
+      Muhammad Ebeid. This site is powered by&nbsp;
+      <a href="https://www.netlify.com/" className="text-indigo-600">
+        Netlify
+      </a>
+    </p>
   </div>
 );
 

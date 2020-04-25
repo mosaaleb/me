52ddef4e398e211dcf919eeb04ea195343b57173
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-04-25 05:38:17 +0200

    update tailwind package

--- src/layouts/SideBar.js
+++ src/layouts/SideBar.js
@@ -13,9 +13,9 @@ const SideBar = ({ isSideBarOpen, setIsSideBarOpen }) => (
       <button
         type="button"
         onClick={() => setIsSideBarOpen(false)}
-        className="block ml-auto p-5 focus:outline-none hover:text-indigo-600 lg:invisible"
+        className="block p-5 ml-auto focus:outline-none hover:text-indigo-600 lg:invisible"
       >
-        <svg className="h-6 w-6 fill-current">
+        <svg className="w-6 h-6 fill-current">
           <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" />
         </svg>
       </button>

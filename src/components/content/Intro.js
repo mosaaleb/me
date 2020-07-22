a8984a5ec3276f24c8ef0257cea79bdf3461c85f
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-07-22 22:49:41 +0200

    fix linter issues

--- src/components/content/Intro.js
+++ src/components/content/Intro.js
@@ -10,8 +10,11 @@ const Intro = () => {
 
   return (
     <div className="pb-12">
-      {items.map(item => (
-        <h3 className="text-3xl font-bold leading-relaxed text-gray-900 lg:text-5xl lg:leading-normal transition-transform duration-500 ease-in-out transform hover:translate-x-3">
+      {items.map((item) => (
+        <h3
+          key={item}
+          className="text-3xl font-bold leading-relaxed text-gray-900 lg:text-5xl lg:leading-normal transition-transform duration-500 ease-in-out transform hover:translate-x-3"
+        >
           {item}
           <span className="text-4xl leading-none text-indigo-600 lg:text-6xl">
             .

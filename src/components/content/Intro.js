35d0e76bf7a94aedf57ed8a7e484fd1d9b556a4a
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-07-22 22:32:07 +0200

    add transition property on into items

--- src/components/content/Intro.js
+++ src/components/content/Intro.js
@@ -1,33 +1,25 @@
 import React from 'react';
 
-const Intro = () => (
-  <div>
-    <h3 className="font-bold text-3xl text-gray-900 leading-relaxed lg:text-5xl lg:leading-normal">
-      Rubyist
-      <span className="text-4xl text-indigo-600 leading-none lg:text-6xl">
-        .
-      </span>
-    </h3>
-    <h3 className="font-bold text-3xl text-gray-900 leading-relaxed lg:text-5xl lg:leading-normal">
-      Engineer
-      <span className="text-4xl text-indigo-600 leading-none lg:text-6xl">
-        .
-      </span>
-    </h3>
+const Intro = () => {
+  const items = [
+    'Rubyist',
+    'Engineer',
+    'Problem Solver',
+    'Full Stack Developer'
+  ];
 
-    <h3 className="font-bold text-3xl text-gray-900 leading-relaxed lg:text-5xl lg:leading-normal">
-      Problem Solver
-      <span className="text-4xl text-indigo-600 leading-none lg:text-6xl">
-        .
-      </span>
-    </h3>
-    <h3 className="font-bold text-3xl text-gray-900 leading-relaxed lg:text-5xl lg:leading-normal">
-      Full Stack Developer
-      <span className="text-4xl text-indigo-600 leading-none lg:text-6xl">
-        .
-      </span>
-    </h3>
-  </div>
-);
+  return (
+    <div className="pb-12">
+      {items.map(item => (
+        <h3 className="text-3xl font-bold leading-relaxed text-gray-900 lg:text-5xl lg:leading-normal transition-transform duration-500 ease-in-out transform hover:translate-x-3">
+          {item}
+          <span className="text-4xl leading-none text-indigo-600 lg:text-6xl">
+            .
+          </span>
+        </h3>
+      ))}
+    </div>
+  );
+};
 
 export default Intro;

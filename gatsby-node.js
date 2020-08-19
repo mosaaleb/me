e6af592929c3c3a507145f9a68d1a15127b85182
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-08-19 23:41:52 +0200

    separate creating pages using filter absolutepath
    use filter: { fileAbsolutePath } to separate creating
    pages for posts and tils

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,7 @@
 exports.createPages = async ({ actions, graphql }) => {
-  const result = await graphql(`
+  const posts = await graphql(`
     query {
-      allMdx {
+      allMdx(filter: {fileAbsolutePath: {regex: "/posts/"}}){
         nodes {
           frontmatter {
             slug
@@ -11,12 +11,34 @@ exports.createPages = async ({ actions, graphql }) => {
     }
   `);
 
-  result.data.allMdx.nodes.forEach((post) => {
+  const tils = await graphql(`
+    query {
+      allMdx(filter: {fileAbsolutePath: {regex: "/tils/"}}){
+        nodes {
+          frontmatter {
+            slug
+          }
+        }
+      }
+    }
+  `);
+
+  posts.data.allMdx.nodes.forEach((post) => {
     actions.createPage({
       path: `/blog/${post.frontmatter.slug}`,
       component: require.resolve('./src/templates/Post.js'),
       context: {
         slug: post.frontmatter.slug
+      }
+    });
+  });
+
+  tils.data.allMdx.nodes.forEach((til) => {
+    actions.createPage({
+      path: `/today-i-learned/${til.frontmatter.slug}`,
+      component: require.resolve('./src/templates/Til.js'),
+      context: {
+        slug: til.frontmatter.slug
       }
     });
   });

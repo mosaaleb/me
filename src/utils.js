e994ff8d41729057dd1d90d83d8703739f74fbb9
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-08-25 18:33:44 +0200

    add function for converting to kebabCase

---
+++ src/utils.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/prefer-default-export */
+
+export const kebabCase = (str) => (
+  str
+    .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+    .map((x) => x.toLowerCase())
+    .join('-')
+);
